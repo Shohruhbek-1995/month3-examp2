7b19609ba4374f6100927df0b0a2892e21e280a9
savobmedia <savobmedia@gmail.com>
2022-09-10 05:37:48 +0500

    examp2

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let sum = 0;
+
+function sumSalaries(salaries){
+	let values = Object.values(salaries);
+	for(const val of values){
+		sum += val;
+	}
+	return sum;
+}
+
+var salaries = {
+	"Akmal": 1200,
+	"Salim": 5200,
+	"Karima": 1800
+}
+
+console.log(sumSalaries(salaries)); // Natija: 8200 -> chiqishi kk
+document.querySelector('.result').innerHTML = "Jami: " + sum;
